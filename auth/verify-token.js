40876da9b42e2246b1421ddbebc67fcdfd279081
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-25 22:25:43 +0700

    add verify token

---
+++ auth/verify-token.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) {
+        return res.status(401).json({ message: 'Authentication failed' })
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid token' })
+        }
+        req.user = user
+        next()
+    })
+}
+
+module.exports = verifyToken
