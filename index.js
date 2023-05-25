b9f003d1dc970c571edb59d2e298b392df86dec3
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-25 22:18:23 +0700

    add routes register & login

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const loginRegisRoutes = require('./routes/login-regis-routes')
 require('dotenv').config()
+
+const app = express()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -8,3 +12,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch((error) => {
         console.log('Error connecting to MongoDB')
     })
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.use('/user', loginRegisRoutes)
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
---
+++ routes/login-regis-routes.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user')
+
+require('dotenv').config()
+
+const router = express.Router()
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+
+        const findExistingEmail = await userModel.findOne({ email })
+        if (findExistingEmail){
+            return res.status(409).json({ message: 'Email already exists' })
+        } 
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new userModel({ username, email, password: hashPassword})
+        await newUser.save()
+
+        res.status(201).json({ message: 'User created successfully' })
+
+    } catch (e) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const user = userModel.findOne({ email })
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' })
+        }
+
+        const matchPassword = bcrypt.compare(password, user.password)
+        if (!matchPassword) {
+            return res.status(401).json({ message: 'Password is wrong'})
+        }
+        
+        const token = jwt.sign({ userId: user._id}, process.env.TOKEN_SECRET)
+
+        res.status(201).json({ message: 'Successful Login', token })
+
+    } catch (e) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
+})
+
+module.exports = router
